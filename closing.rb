597a19b7be153b175e6ed111b94a0456ef776f46
Ikko-T <skyblueboy32@gmail.com>
2020-05-21 19:30:42 +0900

    adding loop syntax

--- closing.rb
+++ closing.rb
@@ -1,12 +1,20 @@
-answer = { y: "はい", n: "いいえ" }
+class Closing
+    def reply
+        answer = { y: "はい", n: "いいえ" }
 
-puts answer
-print "続けますか？"
-response = gets.chomp.to_sym
-puts "#{answer[response]}"
-puts "--------------------------"
+        puts answer
+        print "続けますか？"
+        response = gets.chomp.to_sym
+        puts "#{answer[response]}"
+        puts "--------------------------"
 
-if answer[response] == "はい"
-    # ゲーム終了
-    puts "ゲームを終了します"
+        if answer[response] == "はい"
+            require_relative "if"
+        else
+            puts "ゲームを終了します"
+        end
+    end
 end
+
+close = Closing.new
+close.reply
